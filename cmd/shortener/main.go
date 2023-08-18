6a83aa3b186ef78ce63d0f5e10503846ddc4b0dd
Соколов Максим <sokolov.maksim.1976@mail.ru>
2023-08-18 15:33:17 +0300

    implement logger, reducing, cache

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"hash/crc32"
+	"log"
+)
+
+func main() {
+	str := "Hello, world!"
+	hash := crc32.ChecksumIEEE([]byte(str))
+
+	log.Println(fmt.Sprintf("%x", hash))
+}
